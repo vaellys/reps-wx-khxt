29af47c348a6b9c0467aabbcfeb6c74d5eb6423d
vaellys <vaellys@yahoo.com>
2018-03-20 17:04:04 +0800

    add level person action.

---
+++ reps-khxt-action/src/main/java/com/reps/khxt/action/KhxtLevelWeightAction.java
@@ -0,0 +1,100 @@
+package com.reps.khxt.action;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.reps.core.RepsConstant;
+import com.reps.core.commons.Pagination;
+import com.reps.core.orm.ListResult;
+import com.reps.core.web.AjaxStatus;
+import com.reps.core.web.BaseAction;
+import com.reps.khxt.entity.KhxtCategory;
+import com.reps.khxt.entity.KhxtLevelWeight;
+import com.reps.khxt.service.IKhxtLevelWeightService;
+
+@Controller
+@RequestMapping(value = RepsConstant.ACTION_BASE_PATH + "/khxt/levelweight")
+public class KhxtLevelWeightAction extends BaseAction {
+
+	protected final Logger logger = LoggerFactory.getLogger(KhxtLevelWeightAction.class);
+
+	@Autowired
+	IKhxtLevelWeightService khxtLevelWeightService;
+
+	@RequestMapping(value = "/list")
+	public ModelAndView list(Pagination pager, KhxtLevelWeight khxtLevelWeight) {
+		ModelAndView mav = getModelAndView("/khxt/levelweight/list");
+		ListResult<KhxtLevelWeight> listResult = khxtLevelWeightService.query(pager.getStartRow(), pager.getPageSize(), khxtLevelWeight);
+		// 分页数据
+		mav.addObject("list", listResult.getList());
+		// 分页参数
+		pager.setTotalRecord(listResult.getCount().longValue());
+		mav.addObject("pager", pager);
+		return mav;
+	}
+
+	@RequestMapping(value = "/toadd")
+	public Object toAdd(KhxtCategory khxtCategory) {
+		ModelAndView mav = getModelAndView("/khxt/levelweight/add");
+		try {
+			return mav;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("添加失败", e);
+			return ajax(AjaxStatus.ERROR, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/add")
+	@ResponseBody
+	public Object add(KhxtLevelWeight khxtLevelWeight) {
+		try {
+			khxtLevelWeightService.save(khxtLevelWeight);
+			return ajax(AjaxStatus.OK, "添加成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("添加失败", e);
+			return ajax(AjaxStatus.ERROR, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/toedit")
+	public ModelAndView toEdit(String id) {
+		ModelAndView mav = getModelAndView("/khxt/levelweight/edit");
+		KhxtLevelWeight khxtLevelWeight = khxtLevelWeightService.get(id);
+		mav.addObject("levelWeight", khxtLevelWeight);
+		return mav;
+	}
+
+	@RequestMapping(value = "/edit")
+	@ResponseBody
+	public Object edit(KhxtLevelWeight khxtLevelWeight) {
+		try {
+			khxtLevelWeightService.update(khxtLevelWeight);
+			return ajax(AjaxStatus.OK, "修改成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("修改失败", e);
+			return ajax(AjaxStatus.ERROR, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/delete")
+	@ResponseBody
+	public Object delete(KhxtLevelWeight khxtLevelWeight) {
+		try {
+			khxtLevelWeightService.delete(khxtLevelWeight);
+			return ajax(AjaxStatus.OK, "删除成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除失败", e);
+			return ajax(AjaxStatus.ERROR, e.getMessage());
+		}
+	}
+
+}
